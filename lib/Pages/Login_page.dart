108bc404cf70033fda31d63237f92fbcec681601
Prajapati Hiten <prajapatihiten386@gmail.com>
2025-04-29 22:44:25 +0530

    Initial commit

---
+++ lib/Pages/Login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login_page extends StatefulWidget {
+  const Login_page({super.key});
+
+  @override
+  State<Login_page> createState() => _Login_pageState();
+}
+
+class _Login_pageState extends State<Login_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hello"),
+      ),
+    );
+  }
+}
