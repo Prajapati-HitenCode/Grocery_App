f190bbf9b50625eedf509c84b3487585ffec4297
Prajapati Hiten <prajapatihiten386@gmail.com>
2025-05-10 17:58:19 +0530

    Adding Search,Cart,Account Pages.

--- lib/Pages/Search/Search_bloc/search_bloc.dart
+++ lib/Pages/Search/Search_bloc/search_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:groceryapp/Data/product_model.dart';
 import 'package:meta/meta.dart';
 
 part 'search_event.dart';
@@ -16,6 +18,12 @@ FutureOr<void> searchInitialEvent(
     SearchInitialEvent event, Emitter<SearchState> emit) async {
   emit(SearchLoadingState(message: "Loading.."));
   try {
+    QuerySnapshot snapshot =
+    await FirebaseFirestore.instance.collection('product').get();
+
+    // Convert the fetched documents to a list of product_model
+    List<product_model> productList =
+    snapshot.docs.map((doc) => product_model.fromDocument(doc)).toList();
     emit(SearchSuccessState());
   } catch (e) {
     emit(SearchLoadErrorState(error: e.toString()));
